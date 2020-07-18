583b7924ebe07f47ec3c3e10746b35761ce4b1cb
vvash17 <iasnia1234>
2020-07-18 22:35:31 +0400

    completed individual daos

--- app/src/main/java/com/example/androidshowcase/database/daos/ComponentRatingsDao.kt
+++ app/src/main/java/com/example/androidshowcase/database/daos/ComponentRatingsDao.kt
@@ -5,19 +5,25 @@ import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.Query
 import com.example.androidshowcase.database.entities.ComponentRating
-import com.example.androidshowcase.database.entities.Library
 
 @Dao
 interface ComponentRatingsDao {
-    @Query("select * from component_ratings")
-    fun getAllComponentRatings(): List<ComponentRating>
+    //TODO make sure this is not needed
+//    @Query("select * from component_ratings")
+//    fun getAllComponentRatings(): List<ComponentRating>
+//
+//    @Query("select * from component_ratings where id = :componentRatingId limit 1")
+//    fun getComponentRating(componentRatingId: Int): ComponentRating
 
-    @Query("select * from component_ratings where id = :componentRatingId limit 1")
-    fun getLibrary(componentRatingId: Int): ComponentRating
+    @Query("select * from component_ratings cr join components c on c.name = :componentName and c.id = cr.componentId where cr.ratingId = 1")
+    fun getComponentPositiveRatings(componentName: String): List<ComponentRating>
+
+    @Query("select * from component_ratings cr join components c on c.name = :componentName and c.id = cr.componentId where cr.ratingId = 2")
+    fun getComponentNegativeRatings(componentName: String): List<ComponentRating>
 
     @Insert
     fun insertComponentRating(componentRating: ComponentRating)
 
     @Delete
-    fun deleteLibrary(componentRating: ComponentRating)
+    fun deleteComponentRating(componentRating: ComponentRating)
 }
--- app/src/main/java/com/example/androidshowcase/database/daos/ComponentsDao.kt
+++ app/src/main/java/com/example/androidshowcase/database/daos/ComponentsDao.kt
@@ -1,2 +1,23 @@
 package com.example.androidshowcase.database.daos
 
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import com.example.androidshowcase.database.entities.Component
+import com.example.androidshowcase.database.entities.Library
+
+@Dao
+interface ComponentsDao {
+    @Query("select * from components")
+    fun getAllComponents(): List<Component>
+
+    @Query("select * from components where id = :componentId limit 1")
+    fun getComponent(componentId: Int): Component
+
+    @Insert
+    fun insertComponent(component: Component)
+
+    @Delete
+    fun deleteComponent(component: Component)
+}
