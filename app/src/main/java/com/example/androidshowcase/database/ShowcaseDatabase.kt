4ecd0c8a264e0f6361a1582d8a5bfcbc34973be4
vvash17 <iasnia1234>
2020-07-19 09:59:43 +0400

    added singleton database initialization

--- app/src/main/java/com/example/androidshowcase/database/ShowcaseDatabase.kt
+++ app/src/main/java/com/example/androidshowcase/database/ShowcaseDatabase.kt
@@ -1,10 +1,25 @@
 package com.example.androidshowcase.database
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 import com.example.androidshowcase.database.entities.*
 
 @Database(entities = [User::class, Component::class, ComponentRating::class, Library::class, LibraryRating::class, RatingType::class], version = 1)
 abstract class ShowcaseDatabase : RoomDatabase() {
+    companion object {
+        var showcaseDatabase: ShowcaseDatabase? = null
+        fun getInstance(context: Context): ShowcaseDatabase? {
+            synchronized(this) {
+                if (showcaseDatabase == null) {
+                    showcaseDatabase = Room.databaseBuilder(context, ShowcaseDatabase::class.java, "showcase_database")
+                            .fallbackToDestructiveMigration()
+                            .build()
+                }
+                return showcaseDatabase
+            }
+        }
+    }
 
 }
