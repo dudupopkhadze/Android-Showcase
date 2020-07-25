d3b6f044a5fb580ee935b3635229b4955b8df3ec
shotanozadze <snoza17@freeuni.edu.ge>
2020-07-25 23:23:17 +0400

    UI update

--- app/src/main/java/com/example/androidshowcase/ui/libraries/seekBars/DiscreteSeekBarActivity.kt
+++ app/src/main/java/com/example/androidshowcase/ui/libraries/seekBars/DiscreteSeekBarActivity.kt
@@ -3,6 +3,7 @@ package com.example.androidshowcase.ui.libraries.seekBars
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import com.example.androidshowcase.R
 import com.example.androidshowcase.ui.ComponentActivity
@@ -20,5 +21,12 @@ class DiscreteSeekBarActivity : AppCompatActivity(), ComponentActivity {
     override fun start(context: Context) {
         val intent = Intent(context, DiscreteSeekBarActivity::class.java)
         context.startActivity(intent)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId) {
+            android.R.id.home -> onBackPressed()
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
