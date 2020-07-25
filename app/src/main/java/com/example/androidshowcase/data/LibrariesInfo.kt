3d3839b69e3009f4dff1a683a6de55f7cc07ee89
shotanozadze <snoza17@freeuni.edu.ge>
2020-07-25 17:27:41 +0400

    navigation tab bar

--- app/src/main/java/com/example/androidshowcase/data/LibrariesInfo.kt
+++ app/src/main/java/com/example/androidshowcase/data/LibrariesInfo.kt
@@ -3,6 +3,7 @@ package com.example.androidshowcase.data
 import com.example.androidshowcase.ui.ComponentActivity
 import com.example.androidshowcase.ui.libraries.material.button.MaterialButtonsActivity
 import com.example.androidshowcase.ui.libraries.material.textfield.MaterialTextFieldActivity
+import com.example.androidshowcase.ui.libraries.navigations.NavigationTabBarActivity
 import com.example.androidshowcase.ui.libraries.progressBars.CircularProgressBarActivity
 import com.example.androidshowcase.ui.libraries.progressBars.NumberProgressBarActivity
 import com.example.androidshowcase.ui.libraries.progressBars.SmoothProgressBarActivity
@@ -36,14 +37,20 @@ val seekBarComponents = listOf(
         "DiscreteSeekBar"
 )
 
+val navigationComponents = listOf(
+        "NavigationTabBar"
+)
+
 val libraries: Map<String, List<String>> = mapOf("Material" to materialComponents,
         "Progress Bar" to progressBarComponents,
-        "Seek Bar" to seekBarComponents)
+        "Seek Bar" to seekBarComponents,
+        "Navigation" to navigationComponents)
 
 val componentsToClasses: Map<String, ComponentActivity> = mapOf("Buttons" to MaterialButtonsActivity(),
         "TextFields" to MaterialTextFieldActivity(),
         "SmoothProgressBar" to SmoothProgressBarActivity(),
         "CircularProgressBar" to CircularProgressBarActivity(),
         "DiscreteSeekBar" to DiscreteSeekBarActivity(),
-        "NumberProgressBar" to NumberProgressBarActivity())
+        "NumberProgressBar" to NumberProgressBarActivity(),
+        "NavigationTabBar" to NavigationTabBarActivity())
 val markings: List<String> = listOf("pin")
